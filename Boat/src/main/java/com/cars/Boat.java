295876fbd08e54585043977811af1b553e3b2d64
Bănică Dragoș <dragosxp715@gmail.com>
2024-11-25 18:16:52 +0200

    v 1.0.0 - Just created it Boat

---
+++ Boat/src/main/java/com/cars/Boat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cars;
+
+/**
+ *
+ * @author GrayHat667
+ */
+public class Boat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
